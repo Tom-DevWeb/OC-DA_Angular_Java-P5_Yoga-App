ad24421e591f384e17b5d7b680296cc01233f905
Tom ANDRE <tom.andre@soprasteria.com>
2024-11-18 09:21:47 +0100

    feat(STEP-1): Test unitaire du composant login

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -1,29 +1,30 @@
-import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterTestingModule } from '@angular/router/testing';
-import { expect } from '@jest/globals';
-import { SessionService } from 'src/app/services/session.service';
-
-import { LoginComponent } from './login.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {expect} from '@jest/globals';
+import {LoginComponent} from './login.component';
+import {LoginRequest} from "../../interfaces/loginRequest.interface";
+import {AuthService} from "../../services/auth.service";
+import {throwError} from "rxjs";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authServiceMock: jest.Mocked<AuthService>
 
   beforeEach(async () => {
+    authServiceMock = {
+      login: jest.fn()
+    } as any
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [SessionService],
+      providers: [{ provide: AuthService, useValue: authServiceMock}],
       imports: [
-        RouterTestingModule,
         BrowserAnimationsModule,
-        HttpClientModule,
         MatCardModule,
         MatIconModule,
         MatFormFieldModule,
@@ -40,13 +41,30 @@ describe('LoginComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  //TODO: La gestion des erreurs en cas de mauvais login/password
+  //La gestion des erreurs en cas de mauvais login/password
   it('should handle errors when login/password are incorrect', () => {
+    const loginRequest: LoginRequest =
+      { email: 'test@example.com', password: 'wrongPassword' };
 
+    authServiceMock.login.mockReturnValue(
+      throwError(() => new Error('Unauthorized'))
+    )
+
+    component.form.setValue(loginRequest)
+    component.submit()
+
+    expect(component.onError).toBeTruthy()
   });
 
-  //TODO: L'affichage d'erreur en l'absence d'un champ obligatoire
+  //L'affichage d'erreur en l'absence d'un champ obligatoire
   it('should display an error message when a required field is missing', () => {
+    component.onError = true
+    fixture.detectChanges()
+
+    const errorMessage = fixture.nativeElement.querySelector('.error')
+
+    expect(errorMessage).toBeTruthy()
+    expect(errorMessage.textContent).toContain('An error occurred')
 
   });
 });
