83c70dfc2b1b344896e36b4bf99f2334745baece
Tom ANDRE <tom.andre@soprasteria.com>
2024-11-28 16:41:49 +0100

    feat(STEP-2): Test end-to-end delete et update session

--- front/cypress/e2e/delete_session.cy.ts
+++ front/cypress/e2e/delete_session.cy.ts
@@ -1,9 +1,82 @@
 describe('Delete session spec', () => {
   beforeEach(() => {
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/session',
+      },
+      [
+        {
+          "id": 1,
+          "name": "Cours gratuit",
+          "date": "2024-03-22T00:00:00.000+00:00",
+          "teacher_id": 1,
+          "description": "Test desc",
+          "users": [],
+          "createdAt": "2024-11-27T20:09:34",
+          "updatedAt": "2024-11-27T20:09:34"
+        }
+      ]
+    ).as('getSessions');
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/teacher/1',
+      },
+      {
+        "id": 1,
+        "lastName": "DELAHAYE",
+        "firstName": "Margot",
+        "createdAt": "2024-11-15T11:05:38",
+        "updatedAt": "2024-11-15T11:05:38"
+      }
+    ).as('getSession');
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/session/1',
+      },
+      {
+        "id": 1,
+        "name": "Cours gratuit",
+        "date": "2024-03-22T00:00:00.000+00:00",
+        "teacher_id": 1,
+        "description": "Test desc",
+        "users": [],
+        "createdAt": "2024-11-27T20:09:34",
+        "updatedAt": "2024-11-27T20:09:34"
+      }
+    ).as('getSession');
+
+    cy.intercept('DELETE', '/api/session/1', {
+      body: {},
+    }).as('deleteSession')
+
     cy.login();
   })
 
   it('Delete session successfull', () => {
+    cy.wait('@getSessions')
 
+    cy.contains("Detail").click()
+
+    cy.wait('@getSession')
+
+    cy.intercept('GET', '/api/session',
+      { body: []}
+    ).as('getEmptySessions');
+
+    cy.contains("Delete").click()
+
+    cy.wait('@deleteSession')
+
+    cy.wait('@getEmptySessions')
+
+    cy.contains('Session deleted !').should('be.visible');
+
+    cy.url().should('include', '/sessions');
   });
 })
--- front/cypress/e2e/update_session.cy.ts
+++ front/cypress/e2e/update_session.cy.ts
@@ -1,9 +1,92 @@
 describe('Update session spec', () => {
   beforeEach(() => {
+    cy.intercept(
+      {
+        method: 'PUT',
+        url: '/api/session/1',
+      },
+      {
+        "id": 1,
+        "name": "Nouvelle session",
+        "date": "2024-03-22T00:00:00.000+00:00",
+        "teacher_id": 1,
+        "description": "Test desc",
+        "users": [],
+        "createdAt": "2024-11-27T20:09:34",
+        "updatedAt": "2024-11-27T20:09:34"
+      }
+    ).as('updateSession');
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/teacher',
+      },
+      {
+        "id": 1,
+        "lastName": "DELAHAYE",
+        "firstName": "Margot",
+        "createdAt": "2024-11-15T11:05:38",
+        "updatedAt": "2024-11-15T11:05:38"
+      }
+    ).as('getTeacher');
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/session/1',
+      },
+      {
+        "id": 1,
+        "name": "Cours gratuit",
+        "date": "2024-03-22T00:00:00.000+00:00",
+        "teacher_id": 1,
+        "description": "Test desc",
+        "users": [],
+        "createdAt": "2024-11-27T20:09:34",
+        "updatedAt": "2024-11-27T20:09:34"
+      }
+    ).as('getSession');
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/session',
+      },
+      [
+        {
+          "id": 1,
+          "name": "Cours gratuit",
+          "date": "2024-12-22T00:00:00.000+00:00",
+          "teacher_id": 1,
+          "description": "Test desc1",
+          "users": [],
+          "createdAt": "2024-11-27T20:09:34",
+          "updatedAt": "2024-11-27T20:09:34"
+        }
+      ]
+    ).as('getSessions');
+
     cy.login();
   })
 
   it('Update session successfull', () => {
+    cy.wait('@getSessions')
 
+    cy.contains("Edit").click()
+
+    cy.wait('@getSession')
+
+    cy.wait('@getTeacher')
+
+    cy.get('input[formControlName="name"]').type('{selectall}{backspace}Nouvelle session');
+
+    cy.get('button[type="submit"]').click();
+
+    cy.wait('@updateSession')
+
+    cy.contains('Session updated !').should('be.visible');
+
+    cy.url().should('include', '/sessions');
   });
 })
